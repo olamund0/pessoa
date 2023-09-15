03c07d1353c5c76998239329bd9affcc3ffc7493
Daniel <136710480+olamund0@users.noreply.github.com>
2023-09-15 12:44:57 -0300

    Update printar();

--- Pessoa.java
+++ Pessoa.java
@@ -1,4 +1,4 @@
-package Unifacisa;
+package unifacisa;
 
 import java.util.ArrayList;
 
@@ -30,26 +30,22 @@ public class Pessoa {
 
 		verificarCPF(segundoAmigo, segundoAmigo.CPF);
 
-		
 		// nao adicionei na lista para usar metodo ehAmigoDe
 		Pessoa terceiroAmigo = new Pessoa();
 		terceiroAmigo.nome = "Pedro";
 		terceiroAmigo.idade = 26;
 		terceiroAmigo.CPF = 8879;
-	
+
 		printar();
 
 		amigo.envelhecer(2);
 		System.out.println("Nova idade: ");
-		System.out.println();
-		
+
 		printarIdade(amigo);
-		
 
 		System.out.printf("%s é amigo de %s? " + amigo.ehAmigoDe(segundoAmigo), amigo.nome, segundoAmigo.nome);
 		System.out.printf("\n%s é amigo de %s? " + amigo.ehAmigoDe(terceiroAmigo), amigo.nome, terceiroAmigo.nome);
-		
-		
+
 	}
 
 	public void adicionarAmigo(Pessoa amigo) {
@@ -64,17 +60,17 @@ public class Pessoa {
 
 	public static void printar() {
 		for (Pessoa amigo : listaDeAmigos) {
-			System.out.println(amigo.nome);
-			System.out.println(amigo.idade);
-			System.out.println(amigo.CPF);
+			System.out.println("Nome: " + amigo.nome);
+			System.out.println("Idade: " + amigo.idade);
+			System.out.println("CPF: " + amigo.CPF);
 			System.out.println("");
 		}
 	}
-	
+
 	public static void printarIdade(Pessoa amigo) {
-		System.out.println(amigo.nome);
-		System.out.println(amigo.idade);
-		System.out.println(amigo.CPF);
+		System.out.println("Nome: " + amigo.nome);
+		System.out.println("Idade: " + amigo.idade);
+		System.out.println("CPF: " + amigo.CPF);
 		System.out.println("");
 	}
 
